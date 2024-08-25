46744742f9438c0f34f04bf3ca4d23571a4b9e30
Nihal N <nihalnclt@gmail.com>
2024-08-25 12:06:30 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,224 @@
-import Image from "next/image";
+import Link from "next/link";
+import { FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="h-[70px] ">
+        <div className="w-[1120px] max-w-[100%] h-full mx-auto flex items-center gap-4 justify-between">
+          <h1>WEBYZ</h1>
+          <div className="flex items-center gap-4">
+            <span>Pricing</span>
+            <span>Blog</span>
+            <span>Docs</span>
+          </div>
+          <div>
+            <button>Get Started</button>
+          </div>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="h-[calc(100vh-70px)] flex flex-col justify-between items-center">
+        <div>
+          <h1 className="text-[40px] font-[700] text-center">
+            Our SaaS Solution Will <br />
+            revelutionize your work process
+          </h1>
+          <div className="flex items-center gap-4 justify-center mt-10">
+            <button>Get Started</button>
+            <Link href="/demo">
+              <button>View Demo</button>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src="/images/mac_frame.jpg" alt="" className="w-[900px]" />
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="bg-[#333] w-full h-[300px]"></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+      <div className="mt-10 w-[1120px] max-w-[100%] mx-auto grid grid-cols-2 gap-10">
+        <div>
+          <div>
+            <span>Benefits</span>
+          </div>
+          <h2>Track your sales in a new way</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam culpa quibusdam voluptates
+            ullam ipsa accusamus amet? Nihil deleniti pariatur accusantium ducimus, sint nesciunt
+            exercitationem et deserunt corporis placeat distinctio earum?
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <span></span>
+              <span>Accessibility</span>
+            </div>
+            <div>
+              <span></span>
+              <span>Scalability</span>
+            </div>
+            <div>
+              <span></span>
+              <span>Cost effectiveness</span>
+            </div>
+            <div>
+              <span></span>
+              <span>Customization</span>
+            </div>
+          </div>
+        </div>
+        <div className="w-full p-10 bg-violet-500">
+          <img
+            src="https://cdn.corporatefinanceinstitute.com/assets/line-graph.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
       </div>
-    </main>
+
+      <div className="bg-[#f9fbfd] mt-10">
+        <div className="w-[1120px] max-w-[100%] mx-auto">
+          <div>
+            <span>Features</span>
+          </div>
+          <div className="flex items-start gap-4 justify-between">
+            <h2>How technology is reshaping the workplace is discussed in the future of work</h2>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolor numquam,
+              voluptatem reprehenderit natus vero ex eaque quisquam sint
+            </span>
+          </div>
+          <div className="mt-4 flex items-center gap-4 justify-center">
+            <span>Data analysis</span>
+            <span>Services</span>
+            <span>Marketing</span>
+            <span>Pricing</span>
+          </div>
+          <div className="mt-4 grid grid-cols-2 gap-10">
+            <div></div>
+            <div>
+              <h3>Hashdesk helps to drive business success with big data</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, ex molestiae enim
+                voluptatibus aliquid soluta numquam eos nesciunt ipsam ad cum esse quasi tenetur,
+                cupiditate autem excepturi labore error commodi.
+              </p>
+              <button>Get Started Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-10 w-[1120px] max-w-[100%] mx-auto">
+        <div>
+          <span>Benefits</span>
+        </div>
+        <h2 className="text-center">Traditional SaaS is outdated. Try Hashdesk</h2>
+        <p className="text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos in impedit, dolorum adipisci
+          esse eveniet debitis ullam, natus veritatis ipsa eum maiores perspiciatis.
+        </p>
+        <div className="grid grid-cols-3 gap-5">
+          <div>
+            <h4>Save Time, and icrease productivity</h4>
+            <span>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis, obcaecati.
+            </span>
+            <button>Learn More</button>
+          </div>
+          <div>
+            <h4>Save Time, and icrease productivity</h4>
+            <span>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis, obcaecati.
+            </span>
+            <button>Learn More</button>
+          </div>
+          <div>
+            <h4>Save Time, and icrease productivity</h4>
+            <span>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis, obcaecati.
+            </span>
+            <button>Learn More</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-10 w-[1120px] max-w-[100%] mx-auto">
+        <div className="grid grid-cols-2 gap-10">
+          <div>
+            <div>
+              <span>INTEGRATION</span>
+            </div>
+            <h2>Integrate with your existing tech stack in seconds</h2>
+            <span>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique ut eligendi
+              officia illo eaque, magnam doloremque
+            </span>
+          </div>
+          <button>Explore Integrations</button>
+          <div></div>
+        </div>
+      </div>
+
+      <div className="bg-[#333] w-full h-[300px] mt-10 flex flex-col items-center justify-center">
+        <h4 className="text-white text-2xl font-[600]">Signup for your free 7 day trial now!</h4>
+        <div>
+          <button>Start for free</button>
+          <button>Talk with Experts</button>
+        </div>
+      </div>
+
+      <div className="mt-10 flex items-center justify-center">
+        <span className="font-[600] text-lg">WEBYZ</span>
+      </div>
+
+      <div className="mt-10 border-t">
+        <div className="w-[1120px] max-w-[100%] mx-auto grid grid-cols-4 gap-10">
+          <div>
+            <ul>
+              <li>Platform</li>
+              <li>Instigations</li>
+              <li>Features</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Community</li>
+              <li>Academy</li>
+              <li>Legal</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Blog</li>
+              <li>Docs</li>
+              <li>FAQ</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>About Us</li>
+              <li>Customers</li>
+              <li>Careers</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <div className="border-t  ">
+          <div className="w-[1120px] max-w-[100%] mx-auto flex items-center gap-4 justify-between">
+            <div>
+              <span>{new Date().getFullYear()}. All Rights Reserved</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <FaTwitter />
+              <FaInstagram />
+              <FaLinkedin />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
